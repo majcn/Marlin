4928db3a86d0321729bc9cb36907bc7cbcb84c97
Gregor Majcen <gregor.majcen@sinergise.com>
2020-05-12 20:27:39 +0200

    Z_HYBRID_THRESHOLD from 3 to 5

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2285,10 +2285,10 @@
   #define X2_HYBRID_THRESHOLD    100
   #define Y_HYBRID_THRESHOLD     100
   #define Y2_HYBRID_THRESHOLD    100
-  #define Z_HYBRID_THRESHOLD       3
-  #define Z2_HYBRID_THRESHOLD      3
-  #define Z3_HYBRID_THRESHOLD      3
-  #define Z4_HYBRID_THRESHOLD      3
+  #define Z_HYBRID_THRESHOLD       5
+  #define Z2_HYBRID_THRESHOLD      5
+  #define Z3_HYBRID_THRESHOLD      5
+  #define Z4_HYBRID_THRESHOLD      5
   #define E0_HYBRID_THRESHOLD     30
   #define E1_HYBRID_THRESHOLD     30
   #define E2_HYBRID_THRESHOLD     30
